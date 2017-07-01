6cb343a5f39f085eda0af3414dba323659423299
Milan <hi.milan102@gmail.com>
2017-07-01 07:17:44 -0700

    Initial commit

---
+++ chrome/content_scripts/leavefeedback.js
@@ -0,0 +1,62 @@
+/*
+ * The JavaScript file for the content script
+ * Runs only when on one of two pages:
+ * 1) The list of feedbacks (max 25 feedbacks per page - eBay restriction)
+ * 2) The page that shows up after feedback is left
+ */
+
+/* Check if on feedback page by looking for leave feedback button */
+var feedbackPage = document.getElementById("but_formSave");
+
+/* Check if on page thats shows up after leaving feedback */
+var message = document.querySelector(".arial13bold");
+textContent = message.textContent;
+var number = textContent.match(/\d/g);
+number = number.join("");
+
+/**
+ * Function called if Promise fulfilled
+ * Object has name feedback that corresponds to comment string
+ *
+ * @param  {Object} contents    - The fulfillment value
+ */
+function leaveFeedback(contents) {
+
+  /* Get the string by calling object.Name */
+  var message = contents.feedback;
+
+  /* If we are on the feedback page */
+  if (feedbackPage) {
+
+	  /* Select all radio buttons for leaving positive feedback and click on them */
+	  var positiveRadioButtons = document.querySelectorAll('[id$="-positiveRadio"]');
+
+	  for (i = 0; i < positiveRadioButtons.length; i++) {
+		  positiveRadioButtons[i].click();
+    }
+
+	  /* Set the value of each comment box to the message of the text box that was stored */
+	  for (i = 0; i < positiveRadioButtons.length; i++) {
+		  document.getElementById('comment' + i + i).value = message;
+    }
+
+	  /* Click on leave feedback button */
+	  document.getElementById("but_formSave").click();
+  }
+
+	/* If we are on the page after leaving feedback, then go back to the feedback page
+	 *
+	 * Only goes back if feedback for 25 transactions are left
+	 * 25 is the max number of feedbacks that can be left at one time, implies there are
+	 * more transactions to leave feedback for
+	 */
+	else if (number == 25) {
+		window.location.assign("http://feedback.ebay.com/ws/eBayISAPI.dll?LeaveFeedback2&show_as=all");
+	}
+}
+
+/* Retrieves a Promise object from storage
+ * Pass in null to get entire contents of storage
+ * Pass in callback function that takes storage Objects
+ */
+chrome.storage.local.get(null, leaveFeedback);
---
+++ firefox/popup/ebayfeedback.js
@@ -0,0 +1,33 @@
+/*
+ * The JavaScript file for the popup
+ * Handles user input
+ */
+
+/**
+ * Stores object in local storage
+ * Object has name feedback that corresponds to comment string
+ *
+ * @param  {String} comment    - The input of the text box
+ */
+function storeString(comment) {
+  browser.storage.local.set({feedback : comment});
+}
+
+/* Listen for changes in text box */
+document.addEventListener("change", function(e) {
+
+  /* If textbox is not empty, store the string */
+  if (e.target.value != "") {
+
+	storeString(e.target.value);
+
+	/* After storing, listen for a button click event */
+    document.addEventListener("click", function(e) {
+
+		/* If the button is clicked, open a new tab containing feedback list for all transactions */
+		if (e.target.id === "leave-feedback") {
+			browser.tabs.create({url: "http://feedback.ebay.com/ws/eBayISAPI.dll?LeaveFeedback2&show_as=all"});
+		}
+	});
+ }
+});
